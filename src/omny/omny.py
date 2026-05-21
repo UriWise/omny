565fb4756dc8b5f3526947970bd0e4e74467c5c3
Uri Wise <uriwise@gmail.com>
2026-05-21 19:17:45 +0300

    root dir fix

--- src/omny/omny.py
+++ src/omny/omny.py
@@ -1,9 +1,12 @@
 from mcp.server.fastmcp import FastMCP
-
+import os
 import sys
 
 mcp = FastMCP("LocalNotes")
 notes_file = "notes.txt"
+root_dir = os.path.join(os.path.expanduser("~"), "omny")
+
+
 
 @mcp.tool()
 def add_note_to_file(content: str, filename: str = notes_file) -> str:
@@ -13,7 +16,7 @@ def add_note_to_file(content: str, filename: str = notes_file) -> str:
         filename (str): The name of the file to which the content will be added
         """
     try:
-        with open(filename, 'a') as file:
+        with open(os.path.join(root_dir, filename), 'a') as file:
             file.write(content + '\n')
         return f"Content added to {filename} successfully."
     except Exception as e:
@@ -25,7 +28,7 @@ def add_note_to_file(content: str, filename: str = notes_file) -> str:
 def read_notes(filename: str = notes_file) -> str:
     """Reads the content of the notes file and returns it as a string."""
     try:
-        with open(filename, 'r') as file:
+        with open(os.path.join(root_dir, filename), 'r') as file:
             notes = file.read()
             return notes if notes else "No notes found. The notes file is empty."
     except FileNotFoundError:
@@ -35,4 +38,5 @@ def read_notes(filename: str = notes_file) -> str:
         return f"An error occurred while reading the notes: {e}"
 
 if __name__ == "__main__":
+    print(root_dir)
     mcp.run()
